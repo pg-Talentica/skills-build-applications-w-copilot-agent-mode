cbaf8817125a04a26e51a4824ed892c5f80ebed0
pg-Talentica <prathamesh.gujar@talentica.com>
2026-02-16 15:32:18 +0000

    Add management command to populate octofit_db with test data

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,40 @@
+from django.core.management.base import BaseCommand
+from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **kwargs):
+        # Delete existing data
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Create teams
+        marvel = Team.objects.create(name='marvel')
+        dc = Team.objects.create(name='dc')
+
+        # Create users
+        ironman = User.objects.create(email='ironman@marvel.com', name='Iron Man', team='marvel')
+        captain = User.objects.create(email='captain@marvel.com', name='Captain America', team='marvel')
+        batman = User.objects.create(email='batman@dc.com', name='Batman', team='dc')
+        superman = User.objects.create(email='superman@dc.com', name='Superman', team='dc')
+
+        # Create activities
+        Activity.objects.create(user='Iron Man', activity_type='run', duration=30)
+        Activity.objects.create(user='Captain America', activity_type='cycle', duration=45)
+        Activity.objects.create(user='Batman', activity_type='swim', duration=25)
+        Activity.objects.create(user='Superman', activity_type='fly', duration=60)
+
+        # Create leaderboard
+        Leaderboard.objects.create(team='marvel', points=150)
+        Leaderboard.objects.create(team='dc', points=120)
+
+        # Create workouts
+        Workout.objects.create(name='Pushups', description='Do 20 pushups')
+        Workout.objects.create(name='Situps', description='Do 30 situps')
+        Workout.objects.create(name='Squats', description='Do 40 squats')
+
+        self.stdout.write(self.style.SUCCESS('octofit_db database populated with test data'))
